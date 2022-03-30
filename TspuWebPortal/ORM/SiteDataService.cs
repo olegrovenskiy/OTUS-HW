e481b2fe848ba0a51fd7e606b0065bd27acc72e2
a.garin <a.garin@dcoa.ru>
2022-03-30 17:31:25 +0300

    added entity

--- TspuWebPortal/ORM/SiteDataService.cs
+++ TspuWebPortal/ORM/SiteDataService.cs
@@ -190,5 +190,48 @@ using TspuWebPortal.Model;
         return;
     }
 
-}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    Сущности    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    public List<EntityModelData> ListAllDcEntityModels()
+    {
+        List<EntityModelData>? EntityModelList = _db.EntityModel?.ToList();
+        return EntityModelList;
+    }
+
+    public void CreateEntityModel(EntityModelData objEntityModel)
+    {
+        _db.EntityModel?.Add(objEntityModel);
+        _db.SaveChanges();
+        return;
+    }
+
+    public EntityModelData GetEntityModelInfoById(int ID)
+    {
+
+        EntityModelData? EntityModelInfo = _db.EntityModel?.FirstOrDefault(s => s.EntityModelId == ID);
+
+        if (EntityModelInfo == null)
+        {
+            EntityModelData EntityModelDefaultInfo = new EntityModelData();
+            EntityModelDefaultInfo.EntityModelId = ID;
+            EntityModelDefaultInfo.Vendor = "Не создан";
+            EntityModelDefaultInfo.ModelType = "Не создан";
+            EntityModelDefaultInfo.ModelName = "Не создан";
+            EntityModelDefaultInfo.PartNumber = "Не создан";
+            EntityModelDefaultInfo.NominalPower = 0;
+            EntityModelDefaultInfo.MaximalPower = 0;
+            return EntityModelDefaultInfo;
+        }
+
+        else return EntityModelInfo;
+
+    }
+
+    public void UpdateEntityModelInfo(EntityModelData objDcEntityModel)
+    {
+        _db.EntityModel?.Update(objDcEntityModel);
+        _db.SaveChanges();
+        return;
+    }
+
+}
