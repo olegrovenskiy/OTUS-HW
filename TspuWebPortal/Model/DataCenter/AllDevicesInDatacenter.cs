af9135f4aeb58d90bd5fa334dc2a2581f17be6ef
Виктор Патрухачёв <supervitya@yandex.ru>
2022-07-28 18:57:00 +0300

    Ведомость с разными вкладками

---
+++ TspuWebPortal/Model/DataCenter/AllDevicesInDatacenter.cs
@@ -0,0 +1,8 @@
+﻿namespace TspuWebPortal.Model
+{
+    public class AllDevicesInDatacenter
+    {
+        public List<OuterChassisData>? FullSingleRackData { get; set; }
+        public string DataCenterName { get; set; } = string.Empty;
+    }
+}
--- TspuWebPortal/Model/DataCenter/CollectionRacksInDcData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TspuWebPortal.Model
-{
-    public class AllRacksInDcData
-    {
-        public List<OuterChassisData>? FullSingleRackData { get; set; }
-        public string RackAsbiName { get; set; } = string.Empty;
-        public string RackDcName { get; set; } = string.Empty;
-    }
-}
